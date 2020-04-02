1bef643592f33053c9afdcabcb6bc808d74f76b0
Alessioch <alessiochf@gmail.com>
2020-04-02 17:09:21 +0100

    style changes on box

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -2,13 +2,13 @@
   <div>
     <h2 class="nation">{{ nation }}</h2>
     <v-row align="center" justify="start">
-      <v-col cols="12" md="3">
+      <v-col cols="12" md="4">
         <InfoBox :detail="confirmedCasesNum()" description="Confirmed cases"></InfoBox>
       </v-col>
-      <v-col cols="6" md="3">
+      <v-col cols="6" md="4">
         <InfoBox :detail="deathsNum()" description="Deaths"></InfoBox>
       </v-col>
-      <v-col cols="6" md="3">
+      <v-col cols="6" md="4">
         <InfoBox :detail="'+'+increaseNum()" description="Last 24h"></InfoBox>
       </v-col>
     </v-row>
@@ -159,7 +159,8 @@ export default {
 }
 .box {
   align-items: center;
-  border-radius: 12px;
+  border-radius: 6px;
+  border-bottom: 2px solid #172c61;
   border: 1px solid #dfe0e7;
   display: flex;
   flex-direction: column;
@@ -167,6 +168,7 @@ export default {
   margin: 0;
   padding: 1.3rem 1rem;
   text-align: center;
+  transition: 0.2s all;
   width: auto;
 
   p {
@@ -180,8 +182,12 @@ export default {
     color: #182C61;
   }
 
+  &:hover {
+    box-shadow: 0 0px 2px rgba(0, 0, 0, 0.1), 0 14px 18px rgba(89, 87, 87, 0.1);
+  }
+
   @media (min-width: 690px) {
-    margin: 1rem 0 4rem;
+   margin: 1rem 0 2rem;
   }
 }
 </style>
